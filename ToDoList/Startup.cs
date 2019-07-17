a3fc1fa1b665f1ac4e74fee24f197c7fd84dcde6
Kryklyvets Oleksandr <sashakryklyvets@gmail.com>
2019-07-17 11:43:33 +0300

    502,5 error was resolved

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -16,7 +16,7 @@ namespace ToDoList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {            
-            services.AddMvc;
+            //services.AddMvc;
             services.AddSingleton<IToDoItemService, FakeToDoItemService>();
         }
 
